87bcd51efd76ecff9287d78becedbbbe6fcdcbd6
Vitor Martins <vitor.martins@fever.pt>
2019-08-07 10:49:57 +0100

    ADD Rounting

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 const fs = require('fs')
 const http = require('http')
+const url = require('url')
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, `utf-8`)
 const laptopData = JSON.parse(json);
 
+const server = http.createServer((req, res) => {
+    const pathName = url.parse(req.url, true).pathName
+    const id = url.parse(req.url, true).query.id
+
+    if(pathName === '/products' || pathName === ''){
+        res.writeHead(200, {'Content-type': 'text/html'})
+        res.end('This is the Products page')
+
+    } else if (pathName === '/laptop' && id < laptopData.length) {
+        res.writeHead(200, {'Content-type': 'text/html'})
+        res.end(`This is the Laptop page for laptop ${id}`)
+
+    } else {
+        res.writeHead(404, {'Content-type': 'text/html'})
+        res.end('URL was not found on the server')
+    }
+    
+    res.writeHead(200, {'Content-type': 'text/html'})
+    res.end('This is the response!')
+
+})
+
+server.listen(1337, '127.0.0.1', () => {
+    console.log('Listening for requests now')
+})
